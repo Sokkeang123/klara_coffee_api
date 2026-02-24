5e94d0a280026815d741a9463525279eb5d823a1
Hi i'm sokkeang <sokkeangsieb27225@gmail.com>
2026-02-24 09:45:08 +0700

    Configure Sequelize for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,25 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: "mysql"
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: "mysql"
+// });
+
+// module.exports = sequelize;
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
